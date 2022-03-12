4803ccbc4585694cd8846f4f3907e207b2f1e383
mihsamusev <mih.samusev@gmail.com>
2022-03-12 13:36:58 +0100

    tests  done

--- src/solver.py
+++ src/solver.py
@@ -1,6 +1,31 @@
-def solve_quadratic_eq(a: float, b: float, c: float) -> list:
+import math
+from numbers import Real
+
+
+def _is_usable_real(v: Real):
+    """
+    Checks if number is real, non nan, and non inf
+    """
+    return isinstance(v, Real) and math.isfinite(v)
+
+
+def solve_quadratic_eq(a: Real, b: Real, c: Real) -> tuple:
     """
     Solves quadratic equation ax^2 + bx + c = 0
     Returns a tuple of roots (x1, x2) if solution exists, otherwise ()
     """
-    return ()
+    if not all(map(_is_usable_real, (a, b, c))):
+        raise TypeError("Values a, b and c should be non NaN, non Inf, reals")
+
+    if math.isclose(a, 0):
+        raise ZeroDivisionError("Coefficient 'a' cant be 0")
+
+    disc = b * b - 4 * a * c
+    if disc < 0:
+        return ()
+
+    disc_sqrt = 0 if math.isclose(disc, 0, abs_tol=10e-9) else math.sqrt(disc)
+    x1 = (-b + disc_sqrt) / (2 * a)
+    x2 = (-b - disc_sqrt) / (2 * a)
+    result = (x1, x2)
+    return result
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,5 +1,8 @@
+import math
 import unittest
+from cmath import nan
 
+import pytest
 from src.solver import solve_quadratic_eq
 
 
@@ -8,7 +11,45 @@ class TestSolver(unittest.TestCase):
     Testing equation solver for ax^2 + bx + c = 0
     """
 
-    def test_solve_no_roots(self):
+    def test_solve_zero_roots(self):
         (a, b, c) = (1, 0, 1)
         result = solve_quadratic_eq(a, b, c)
         self.assertEqual(len(result), 0)
+
+    def test_solve_two_different_roots(self):
+        (a, b, c) = (1, 0, -1)
+        result = solve_quadratic_eq(a, b, c)
+        self.assertEqual(len(result), 2)
+        self.assertTrue(math.isclose(result[0], 1))
+        self.assertTrue(math.isclose(result[1], -1))
+
+    def test_solve_two_identical_roots_zero_disc(self):
+        (a, b, c) = (1.0, 2.0, 1.0)
+        result = solve_quadratic_eq(a, b, c)
+        self.assertEqual(len(result), 2)
+        self.assertTrue(math.isclose(result[0], -1))
+        self.assertTrue(math.isclose(result[1], -1))
+
+    def test_solve_two_identical_roots_sub_epsilon_disc(self):
+        sub_eps = 10e-10  # additional sub epsilon value
+        (a, b, c) = (1.0, 2.0 + sub_eps, 1.0)
+        result = solve_quadratic_eq(a, b, c)
+        self.assertEqual(len(result), 2)
+        self.assertTrue(math.isclose(result[0], -1))
+        self.assertTrue(math.isclose(result[1], -1))
+
+    def test_solve_zero_a_raises_division_error(self):
+        (a, b, c) = (0, 2, 1)
+        self.assertRaises(ZeroDivisionError, solve_quadratic_eq, a, b, c)
+
+    def test_solve_non_real_type_raises(self):
+        (a, b, c) = (1, 2, 1)
+
+        # non real types
+        self.assertRaises(TypeError, solve_quadratic_eq, "1", b, c)
+        self.assertRaises(TypeError, solve_quadratic_eq, a, "2", c)
+        self.assertRaises(TypeError, solve_quadratic_eq, a, b, "1")
+
+        # types that can be interpreted as real
+        self.assertRaises(TypeError, solve_quadratic_eq, math.nan, b, c)
+        self.assertRaises(TypeError, solve_quadratic_eq, math.inf, b, c)
